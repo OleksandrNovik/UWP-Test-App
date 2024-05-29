f61868d3153a152efabfb8dc7e94d021034c5c73
Novik Oleksandr <novikoleksandr9@gmail.com>
2024-05-29 10:54:01 +0300

    Created List View
    Now user list can be viewed and has options to interact with. Edit and Delete user from list is now available (only from UI).

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using SecondApp.ViewModels;
+using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -12,6 +13,7 @@ namespace SecondApp
         public MainPage()
         {
             this.InitializeComponent();
+            this.DataContext = new MainPageViewModel();
         }
     }
 }
---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using SecondApp.Models;
+using System.Collections.ObjectModel;
+
+namespace SecondApp.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public string CurrentFirstName { get; set; }
+
+        public string CurrentLastName { get; set; }
+
+        public ObservableCollection<UserModel> Users { get; set; }
+
+        public MainPageViewModel()
+        {
+            Users = new ObservableCollection<UserModel>()
+            {
+                new UserModel { FirstName = "FirstName", LastName = "LastName" }
+            };
+        }
+    }
+}
